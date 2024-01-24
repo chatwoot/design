8764472aec04f92311b2cc277002069e4e2ca4b1
Nithin David Thomas <1277421+nithindavid@users.noreply.github.com>
2024-01-24 23:30:22 +0530

    feat: Component for tag component

---
+++ src/components/Tag/Tag.story.vue
@@ -0,0 +1,58 @@
+<script setup>
+import Tag from './Tag.vue';
+</script>
+
+<template>
+  <Story title="Conversation/Tag">
+    <Variant title="Medium sized TEXT only tag">
+      <Tag>hello</Tag>
+    </Variant>
+    <Variant title="Medium sized SYMBOL-TEXT tag">
+      <Tag icon="i-fluent-clock-16-regular">hello</Tag>
+    </Variant>
+    <Variant title="Medium sized COLOR-TEXT tag">
+      <Tag color="red">hello</Tag>
+    </Variant>
+    <Variant title="Medium sized SYMBOL ONLY tag">
+      <Tag icon="i-fluent-clock-16-regular"></Tag>
+    </Variant>
+    <Variant title="Medium sized ACTION tag">
+      <Tag removable></Tag>
+    </Variant>
+    <Variant title="Medium sized ACTION tag">
+      <Tag expandable></Tag>
+    </Variant>
+    <Variant title="Medium sized TEXT-ACTION tag">
+      <Tag expandable disabled>paid customer</Tag>
+    </Variant>
+    <Variant title="Medium sized SYMBOL-TEXT-ACTION tag">
+      <Tag expandable icon="i-fluent-clock-16-regular" clickable>clickable</Tag>
+    </Variant>
+
+    <Variant title="Small sized TEXT only tag">
+      <Tag size="s">hello</Tag>
+    </Variant>
+    <Variant title="Small sized SYMBOL-TEXT tag">
+      <Tag size="s" icon="i-fluent-clock-16-regular">hello</Tag>
+    </Variant>
+    <Variant title="Small sized COLOR-TEXT tag">
+      <Tag size="s" color="red">hello</Tag>
+    </Variant>
+    <Variant title="Small sized SYMBOL ONLY tag">
+      <Tag size="s" icon="i-fluent-clock-16-regular"></Tag>
+    </Variant>
+    <Variant title="Small sized ACTION tag">
+      <Tag size="s" removable></Tag>
+    </Variant>
+    <Variant title="Small sized ACTION tag">
+      <Tag size="s" expandable></Tag>
+    </Variant>
+    <Variant title="Small sized TEXT-ACTION tag">
+      <Tag size="s" expandable>paid customer</Tag>
+    </Variant>
+    <Variant title="Small sized SYMBOL-TEXT-ACTION tag">
+      <Tag size="s" expandable icon="i-fluent-clock-16-regular">paid customer</Tag>
+    </Variant>
+    
+  </Story>
+</template>
---
+++ src/components/Tag/Tag.vue
@@ -0,0 +1,89 @@
+<script lang="ts" setup>
+import { ref, defineProps, defineEmits, useSlots } from 'vue';
+import { TagProps } from './types';
+
+const props = withDefaults(defineProps<TagProps>(), {
+  size: 'm',
+  style: 'outlined',
+  disabled: false,
+  clickable: false,
+  removable: false,
+});
+
+const slots = useSlots();
+let containerClasses = '';
+const hasContent = slots.default;
+const hasSymbol = props.icon || props.color;
+const hasAction = props.removable || props.expandable;
+
+// Check if the 'color' prop is provided
+if (props.size === 's') {
+  containerClasses += '  text-xs h-5 min-w-[20px] py-0.5 rounded ';
+
+  // Variant: SYMBOL-TEXT
+  if (hasSymbol && hasContent && !hasAction) {
+    containerClasses += 'pr-1 ';
+  }
+  // Variant: TEXT-ACTION
+  else if (!hasSymbol && hasContent && hasAction) {
+    containerClasses += 'pl-1 ';
+  }
+
+  // Variant: TEXT
+  if (hasContent) {
+    containerClasses += 'px-1 ';
+  }
+} else if (props.size === 'm') {
+  containerClasses += ' text-sm h-6 min-w-[24px] leading-4 p-1 rounded-md ';
+
+  // Variant: SYMBOL-TEXT
+  if (hasSymbol && hasContent && !hasAction) {
+    containerClasses += 'pr-2 ';
+  }
+  // Variant: TEXT-ACTION
+  else if (!hasSymbol && hasContent && hasAction) {
+    containerClasses += 'pl-2 ';
+  }
+  // Variant: TEXT
+  else if (!hasSymbol && hasContent && !hasAction) {
+    containerClasses += 'px-2 ';
+  }
+}
+const allStyleClasses = {
+  ghost: 'outline-transparent',
+  outlined: 'outline-slate-500',
+};
+const emits = defineEmits(['action', 'click']);
+const disabledClasses = ref(props.disabled ? 'opacity-50' : '');
+const disabledActionClasses = ref(props.disabled ? 'cursor-default' : 'hover:bg-slate-400');
+const clickableClasses = ref(props.clickable ? 'cursor-pointer hover:bg-slate-300 outline-slate-300' : '');
+// const tagSizeClasses = ref(allSizeClasses[props.size]);
+const styleClasses = ref(allStyleClasses[props.style]);
+
+// const variantClasses = ref(props.emoji || props.icon || props.color ? '' : 'pl-2');
+const actionIcon = ref(props.removable ? 'i-fluent-dismiss-12-regular' : 'i-fluent-chevron-down-12-regular');
+</script>
+
+<template>
+  <div
+    class="inline-flex items-center justify-center gap-0.5 text-slate-1100 outline outline-1 outline-solid outline-offset-[-1px]"
+    :class="[containerClasses, styleClasses, clickableClasses, disabledClasses]"
+    @click="props.clickable && !props.disabled && $emit('action')"
+  >
+    <span
+      v-if="props.color"
+      class="w-4 h-4 inline-block border-[3px] border-white rounded-md"
+      :style="{ background: props.color }"
+    ></span>
+    <span v-else-if="props.icon" class="w-4 h-4 inline-block" :class="props.icon"></span>
+    <slot></slot>
+    <button
+      v-if="props.removable || props.expandable"
+      class="w-[14px] h-[14px] inline-flex justify-center items-center rounded"
+      :class="[disabledActionClasses]"
+      @click="!props.disabled && $emit('action')"
+    >
+      <span class="text-xs" :class="actionIcon"></span>
+    </button>
+  </div>
+</template>
